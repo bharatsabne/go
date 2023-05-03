02cc822b0b710d9f06e087b1778c09528d2e57aa
Bharat Sabne <bharatsabne1991@gmail.com>
2023-05-03 22:40:43 +0530

    s
    x

--- Study/main.go
+++ Study/main.go
@@ -10,10 +10,10 @@ func main() {
 	fmt.Println(someVariable)
 	i = 9987275806
 	fmt.Println("I Value :", i)
-	returnFromString, otherThingFromFunction := SomeFunction()
-	fmt.Println("Function returned: ", returnFromString, otherThingFromFunction)
+	returnFromString, otherThingFromFunction, yetAnotherString := SomeFunction()
+	fmt.Println("Function returned: ", returnFromString, otherThingFromFunction, yetAnotherString)
 }
 
-func SomeFunction() (string, string) {
-	return "Something!", "LOL"
+func SomeFunction() (string, string,string) {
+	return "Something!", "LOL", "Yet another string"
 }
--- Study/structure.go
+++ Study/structure.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 )
@@ -13,6 +14,13 @@ type User struct {
 	Birthday    time.Time
 }
 
+func (u *User) Add() User {
+	u.FirstName = "India"
+	u.LastName = "Sabne"
+	u.Age = 31
+	u.PhoneNumber = "9987275806"
+	return *u
+}
 func main() {
 	user := User{
 		FirstName:   "Bharat",
@@ -21,5 +29,13 @@ func main() {
 		Age:         31,
 		Birthday:    time.Date(1991, 8, 15, 00, 00, 00, 000, time.Local),
 	}
-	log.Println(user.FirstName, user.LastName, user.Birthday)
+	log.Println(user.FirstName, user.LastName, user.Birthday, user.Age)
+	user = user.Add()
+	log.Println(user.FirstName, user.LastName, user.Birthday, user.Age)
+
+	myMap := make(map[string]string)
+	myMap["Name"] = "Bharat"
+	myMap["LastName"] = "Sabne"
+
+	fmt.Println(myMap["Name"], myMap["LastName"])
 }
